1ebbc31a221e11dc01926d90bcae155b5220cb69
Patrick Chin <p.ch@live.co.uk>
2015-09-08 04:00:10 +0100

    reduced number of copied strings

---
+++ exoplanet.hpp
@@ -0,0 +1,86 @@
+// #pragma once
+
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class exoplanet
+{
+    string name_;
+    int year_;
+    string method_;
+    double mass_, separation_, distance_;
+
+    exoplanet(string &name_, int year_, string &method_, double mass_,
+            double separation_, double distance_)
+        : name_(name_), year_(year_), method_(method_), mass_(mass_),
+            separation_(separation_), distance_(distance_) { }
+
+public:
+    const string name() { return this->name_; }
+    const int year() { return this->year_; }
+    const string method() { return this->method_; }
+    const double mass() { return this->mass_; }
+    const double separation() { return this->separation_; }
+    const double distance() { return this->distance_; }
+
+    static exoplanet from_string(string::const_iterator begin, string::const_iterator end)
+    {
+        string::const_iterator pos = begin;
+        while (*pos != ',') { ++pos; }
+        string name(begin, pos);
+
+        begin = ++pos;
+        while (*pos != ',') { ++pos; }
+        int year = atoi(&(*begin));
+
+        begin = ++pos;
+        while (*pos != ',') { ++pos; }
+        string method(begin, pos);
+
+        begin = ++pos;
+        while (*pos != ',') { ++pos; }
+        double mass = atof(&(*begin));
+
+        begin = ++pos;
+        while (*pos != ',') { ++pos; }
+        double separation = atof(&(*begin));
+
+        begin = ++pos;
+        while (pos != end) { ++pos; }
+        double distance = atof(&(*begin));
+
+        return exoplanet(name, year, method, mass, separation, distance);
+    }
+
+    friend ostream &operator<<(ostream &os, const exoplanet p);
+};
+
+class exoplanet_list
+{
+public:
+    exoplanet_list (const string &s)
+    {
+        int n = count(s.cbegin(), s.cend(), '\n');
+        v.reserve(n+1);
+    }
+
+private:
+    vector<exoplanet> v;
+};
+
+ostream &operator<<(ostream &os, const exoplanet p)
+{
+    return os << p.name_ << endl
+        << "    Year of discovery: " << p.year_ << endl
+        << "    Method of discovery: " << p.method_ << endl
+        << "    Mass (Jupiter Masses): " << p.mass_ << endl
+        << "    Separation (AU): " << p.separation_ << endl
+        << "    Distance from Earth (pc): " <<
+                (p.distance_ == -1 ? "Unknown" : to_string(p.distance_)) << endl;
+}
+
--- main.cpp
+++ main.cpp
@@ -6,73 +6,31 @@
 #include <limits>
 #include <vector>
 #include <ctime>
+#include <fstream>
 
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
+#include "exoplanet.hpp"
+
 using namespace std;
 #define DEBUG(x) cout << ">" << #x << ": " << x << endl
-
-struct exoplanet
-{
-    // enum discovery_method { direct_imaging, transit, radial_velocity, microlensing, timing };
-
-    const string name;
-    const int year;
-    const string method; // of discovery
-    const double mass, separation, distance = -1;
-
-    exoplanet (string name, int year, string method, double mass,
-            double separation, double distance = -1) :
-        name(name), year(year), method(method), mass(mass),
-        separation(separation), distance(distance) { }
-
-    static exoplanet from_string(string s, char delim = ',')
-    {
-        string tmp;
-        stringstream ss(s);
-
-        getline(ss, tmp, delim);
-        string name = tmp.c_str();
-
-        getline(ss, tmp, delim);
-        int year = stoi(tmp);
-
-        getline(ss, tmp, delim);
-        string method = tmp.c_str();
-
-        getline(ss, tmp, delim);
-        double mass = stod(tmp);
-
-        getline(ss, tmp, delim);
-        double sep = stod(tmp);
-
-        getline(ss, tmp, delim);
-        double dist = tmp.size() > 1 ? stod(tmp) : -1; // accounting for the '\0' string ending
-
-        return exoplanet(name,year,method,mass,sep,dist);
-    }
-};
-
-ostream& operator<<(ostream& os, const exoplanet p)
-{
-    return os << p.name << endl
-        << "    Year of discovery: " << p.year << endl
-        << "    Method of discovery: " << p.method << endl
-        << "    Mass (Jupiter Masses): " << p.mass << endl
-        << "    Separation (AU): " << p.separation << endl
-        << "    Distance from Earth (pc): " << (p.distance == -1 ? "Unknown" : to_string(p.distance)) << endl;
-}
 
 int main(void)
 {
     clock_t t1 = clock();
 
+
+    stringstream response_stream;
+
+    // response_stream <<  "NAMEDATE,PLANETDISCMETH,MASS,SEP,DIST\n"
+    //                     "Kepler-107 d,2014,Transit,0.00371234,0.0780099,\n"
+    //                     "WASP-14 b,2009,Transit,7.69234,0.0367693,160\n"
+    //                     "Kepler-50 b,2011,Transit,0.0164915,0.0825598,\n"
+    //                     "NN Ser d,2010,Timing,0.642621,3.70596,\n";
+
     curlpp::Easy request;
-    stringstream response;
-
-    request.setOpt(curlpp::Options::WriteStream(&response)); // where to write the response to
-
+    request.setOpt(curlpp::Options::WriteStream(&response_stream)); // where to write the response to
     try {
         request.setOpt(curlpp::Options::Url("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/exoplanets.txt"));
         request.perform();
@@ -87,14 +45,14 @@ int main(void)
 
     try {
         string line;
-        response.ignore(numeric_limits<streamsize>::max(),'\n'); // ignore first line
-        while (getline(response, line))
+        response_stream.ignore(numeric_limits<streamsize>::max(),'\n'); // ignore first line
+        while (getline(response_stream, line))
         {
-            exoplanet p = exoplanet::from_string(line);
+            exoplanet p = exoplanet::from_string(line.begin(), line.end());
             v.push_back(p);
         }
     } catch (exception &e) {
-        cerr << e.what() << endl;
+        cerr << " ERROR: " << e.what() << endl;
         return 0;
     }
 
@@ -106,67 +64,68 @@ int main(void)
         int i = 0, sz = v.size();
         // find first planet with a known distance
         for (; i < sz; ++i)
-            if (v[i].distance >= 0.0)
+            if (v[i].distance() >= 0.0)
             {
                 closest = &v[i];
                 break;
             }
         // compare remaining planets
         for (; i < sz; ++i)
-            if (v[i].distance >= 0.0 && v[i].distance < closest->distance)
+            if (v[i].distance() >= 0.0 && v[i].distance() < closest->distance())
                 closest = &v[i];
 
         if (closest)
-            cout << *closest << endl;
+            cout << "Closest planet:\n" << *closest << endl;
     }
 
     clock_t t4 = clock();
 
     /* Get stats for discovery methods */
+    struct discovery_method_stats {
+        int count = 0;
+        int earliest_year;
+        double lightest_mass;
+    };
+
+    map<string,discovery_method_stats> count;
+    for (auto p : v)
     {
-        struct discovery_method_stats {
-            int count = 0;
-            int earliest_year;
-            double lightest_mass;
-        };
-
-        map<string,discovery_method_stats> count;
-        for (auto p : v)
+        auto *cur_method = &count[p.method()];
+        if (++(*cur_method).count == 1)
         {
-            auto *cur_method = &count[p.method];
-            if (++(*cur_method).count == 1)
-            {
-                /* new method discovered */
-                (*cur_method).earliest_year = p.year;
-                (*cur_method).lightest_mass = p.mass;
-            }
-            else
-            {
-                if (p.year < (*cur_method).earliest_year)
-                    (*cur_method).earliest_year = p.year;
+            /* new method discovered */
+            (*cur_method).earliest_year = p.year();
+            (*cur_method).lightest_mass = p.mass();
+        }
+        else
+        {
+            if (p.year() < (*cur_method).earliest_year)
+                (*cur_method).earliest_year = p.year();
 
-                if (p.mass < (*cur_method).lightest_mass)
-                    (*cur_method).lightest_mass = p.mass;
-            }
-            
+            if (p.mass() < (*cur_method).lightest_mass)
+                (*cur_method).lightest_mass = p.mass();
         }
 
-        for (auto m : count)
-        {
-            cout << "Method of discovery: " << m.first << endl
-                << "    Planets discovered: " << m.second.count << endl
-                << "    Earliest discovery: " << m.second.earliest_year << endl
-                << "    Lightest planet:    " << m.second.lightest_mass << endl << endl;
-        }
     }
 
     clock_t t5 = clock();
+
+    for (auto m : count)
+    {
+        cout << "Method of discovery: " << m.first << endl
+            << "    Planets discovered: " << m.second.count << endl
+            << "    Earliest discovery: " << m.second.earliest_year << endl
+            << "    Lightest planet:    " << m.second.lightest_mass << endl << endl;
+    }
+
+    clock_t t6 = clock();
 
     std::cout << fixed << setprecision(2)
               << "t1: " << 1000.0 * (t2-t1) / CLOCKS_PER_SEC << " ms\n" << endl
               << "t2: " << 1000.0 * (t3-t2) / CLOCKS_PER_SEC << " ms\n" << endl
               << "t3: " << 1000.0 * (t4-t3) / CLOCKS_PER_SEC << " ms\n" << endl
-              << "t4: " << 1000.0 * (t5-t4) / CLOCKS_PER_SEC << " ms\n" << endl;
+              << "t4: " << 1000.0 * (t5-t4) / CLOCKS_PER_SEC << " ms\n" << endl
+              << "t5: " << 1000.0 * (t6-t5) / CLOCKS_PER_SEC << " ms\n" << endl;
 
     return 0;
 }
